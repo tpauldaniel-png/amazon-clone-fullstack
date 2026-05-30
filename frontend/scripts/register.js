42c45f7c3097c5120f9cc44b5934ca99a515f09b
Paul Daniel <tpauldaniel@gmail.com>
2026-05-30 10:46:10 +0530

    made register form data post into database

---
+++ frontend/scripts/register.js
@@ -0,0 +1,41 @@
+
+
+const form = document.querySelector("#custom-form")
+
+
+
+async function sendData() {
+    
+    const formData = new FormData(form);
+
+    const formValues = Object.fromEntries(formData);
+
+    const sentForm = JSON.stringify(formValues);
+    
+    try {
+        const response = await fetch("http://127.0.0.1:8000/users/", {
+            method: "POST",
+            body: sentForm,
+            headers: {
+                'Content-Type' : 'application/json'
+            }
+
+        });
+        console.log(await response.json());
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+
+
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    sendData();
+
+
+});
+
